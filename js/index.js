e1965017665a924bb5a0991be61d601c752c2c44
amiemoreno <68307446+amiemoreno@users.noreply.github.com>
2021-01-17 15:37:08 -0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("body").hide().fadeIn(1500)
+
+    // adapted from https://codepen.io/_codemics/pen/PwEbYJ
+    var stickyNavTop = $('nav').offset().top;
+
+
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > stickyNavTop) {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    });
+});
